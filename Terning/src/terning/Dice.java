810d6f5f8c3ccd75426d4907a7e3004169a006bd
KimSand <s@ndberg.dk>
2017-10-02 15:55:02 +0200

    Lavet test og sørget for at der er en statistisk korrekt spredning.

---
+++ Terning/src/terning/Dice.java
@@ -0,0 +1,22 @@
+package terning;
+
+public class Dice {
+	// roll the die and return the value (1-6)
+	public Dice() {
+		
+	}
+	
+	public int roll() {
+		float d1=(float)Math.random();     // 0-1
+		float d2=d1*6;                     // 0-5
+		int d3=(int)d2;             // 0-5 integer
+		return d3+1;                     // 1-6		
+	}
+	
+	// roll the die n times and print the values
+	public void rollMultiple(int n) {
+		for (int i=1; i<n; i++) {
+			System.out.print(roll() + " ");
+		}		
+	}
+}
